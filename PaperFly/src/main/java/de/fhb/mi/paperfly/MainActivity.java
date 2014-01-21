b91abf632c01f80f7cb7a7c81d1b61f86c481689
kaain <ott@fh-brandenburg.de>
2014-01-21 23:05:41 +0100

    - stop chatService when logging out

--- PaperFly/src/main/java/de/fhb/mi/paperfly/MainActivity.java
+++ PaperFly/src/main/java/de/fhb/mi/paperfly/MainActivity.java
@@ -722,6 +722,7 @@ public class MainActivity extends Activity implements GetRoomAsyncDelegate {
             if (success) {
                 finish();
             }
+            stopService(new Intent(MainActivity.this, ChatService.class));
         }
     }
 
